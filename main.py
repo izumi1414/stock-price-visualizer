dc3948849be4bcf243664ee85232fcb32e8ba9ab
izumi.takeuchi <izumitku99@gmail.com>
2026-07-20 16:51:21 +0900

    feat: add stock price chart and sidebar controls for date range

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import yfinance as yf
 import streamlit as st
+import altair as alt
 
+st.title('米国株価可視化アプリ')
+
+st.sidebar.write("""
+    # GAFA株価
+    こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。                 
+""")
+
+st.sidebar.write("""
+    ## 表示日数選択
+""")
+
+days = st.sidebar.slider("日数", 1, 50, 20)
+
+st.write(f"""
+    ### 過去{days}日間のGAFA株価
+""")
+
+@st.cache_data
 def get_data(days, tickers):
     df = pd.DataFrame()
     for company in tickers.keys():
@@ -16,9 +34,14 @@ def get_data(days, tickers):
         df = pd.concat([df, hist])
     return df  
 
-days = 20
+st.sidebar.write("""
+    ## 株価の範囲を指定
+""")
+
+ymin, ymax = st.sidebar.slider("株価の範囲を指定してください", 0.0, 3500.0, (0.0, 3500.0))
+
 tickers = {
-    'Apple': 'AAPL',
+    'apple': 'AAPL',
     'facebook': 'FB',
     'google': 'GOOGL',
     'microsoft': 'MSFT',
@@ -28,4 +51,28 @@ tickers = {
 
 df = get_data(days, tickers)
 
-st.write(df)
+companies = st.multiselect(
+    '会社名を選択してください',
+    list(df.index),
+    ['google', 'amazon', 'facebook', 'apple']
+)
+
+if not companies:
+    st.error("少なくとも1社は選んでください")
+else:
+    data = df.loc[companies]
+    st.write("### 株価(USD)", data.sort_index())
+    data = data.T.reset_index()
+    data = pd.melt(data, id_vars=['Date']).rename(
+        columns={'value': 'Stock Prices(USD)'}
+    )
+    chart = (
+        alt.Chart(data)
+        .mark_line(opacity=0.8, clip=True)
+        .encode(
+            x="Date:T",
+            y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+            color='Name:N'
+        )
+    )
+    st.altair_chart(chart, use_container_width=True)
